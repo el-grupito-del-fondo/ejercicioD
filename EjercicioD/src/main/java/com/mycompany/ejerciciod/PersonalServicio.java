6f5eeaf4f66e39f1f2366eab346da93c70d88c41
Alvaro Alvarez <43978223+AlvarezAO@users.noreply.github.com>
2020-02-12 12:03:45 -0300

    Update PersonalServicio.java
    Creacion de atributos mas Constructores

--- EjercicioD/src/main/java/com/mycompany/ejerciciod/PersonalServicio.java
+++ EjercicioD/src/main/java/com/mycompany/ejerciciod/PersonalServicio.java
@@ -5,10 +5,29 @@
  */
 package com.mycompany.ejerciciod;
 
-/**
- *
- * @author Alumno
- */
-public class PersonalServicio {
-    
+package com.mycompany.ejerciciod;
+
+public class PersonalServicio extends Persona {
+	
+	private String seccion;
+	
+	public PersonalServicio(String nombre, String apellidos, String id, String estadoCivil,
+							String seccion) {
+		super(nombre, apellidos, id, estadoCivil);
+		
+		this.seccion = seccion;
+	}
+	
+	public void setSeccion(String seccion) {
+		this.seccion = seccion;
+	}
+	
+	public String getSeccion() {
+		return seccion;
+	}
+							
+	public String toString() {
+		return super.toString() + "Seccion: "+ seccion;
+		
+	}
 }
