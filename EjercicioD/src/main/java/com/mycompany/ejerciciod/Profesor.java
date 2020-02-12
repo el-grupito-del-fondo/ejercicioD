95c2e2012c0237fac7877c54a464e97ed84fcb70
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2020-02-12 11:56:03 -0300

    Update Profesor

--- EjercicioD/src/main/java/com/mycompany/ejerciciod/Profesor.java
+++ EjercicioD/src/main/java/com/mycompany/ejerciciod/Profesor.java
@@ -9,6 +9,35 @@ package com.mycompany.ejerciciod;
  *
  * @author Alumno
  */
-public class Profesor {
-    
+public class Profesor extends Persona {
+	
+	//ATRIBUTO
+    public String departamento;
+
+    //CONSTRUCTOR
+	public Profesor(String nombre, String apellidos, String id, String estadoCivil, String departamento) {
+		super(nombre, apellidos, id, estadoCivil);
+		this.departamento = departamento;
+	}
+
+	//SETT GETT
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	//toSTRING
+	@Override
+	public String toString() {
+		return super.toString() + "Profesor [departamento=" + departamento + "]";
+	}
+
+	//METODOS
+	  public void cambiarDepartamento(String dep){
+	        this.departamento = dep;
+	    }
+	
 }
