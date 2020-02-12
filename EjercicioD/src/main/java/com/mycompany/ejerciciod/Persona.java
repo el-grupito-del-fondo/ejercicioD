93e728c18996c1062c60fe757ed4d66c4a96c615
Alumno <Alumno@PC-PC>
2020-02-12 10:38:40 -0300

    nuevo ingreso de proyecto

--- EjercicioD/src/main/java/com/mycompany/ejerciciod/Persona.java
+++ EjercicioD/src/main/java/com/mycompany/ejerciciod/Persona.java
@@ -10,5 +10,59 @@ package com.mycompany.ejerciciod;
  * @author Alumno
  */
 public class Persona {
+    String nombre;
+    String apellidos;
+    String id;
+    String estadoCivil;
+
+    public Persona(String nombre, String apellidos, String id, String estadoCivil) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.id = id;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", id=" + id + ", estadoCivil=" + estadoCivil + '}';
+    }
     
+    public void cambiarEstadoCivil(String est){
+        this.estadoCivil = est;
+    }
 }
