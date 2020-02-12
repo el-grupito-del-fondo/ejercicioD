b4c9491d3f6dfce48252b54fe01b1d963737cf0e
Alumno <Alumno@PC-PC>
2020-02-12 10:26:10 -0300

    nuevo ingreso de proyecto

--- EjercicioD/src/main/java/com/mycompany/ejerciciod/Alumno.java
+++ EjercicioD/src/main/java/com/mycompany/ejerciciod/Alumno.java
@@ -10,5 +10,57 @@ package com.mycompany.ejerciciod;
  * @author Alumno
  */
 public class Alumno {
+    String nombre;
+    String apellidos;
+    String id;
+    String estadoCivil;
+
+    public Alumno(String nombre, String apellidos, String id, String estadoCivil) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.id = id;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Alumno() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", id=" + id + ", estadoCivil=" + estadoCivil + '}';
+    }
+    
     
 }
